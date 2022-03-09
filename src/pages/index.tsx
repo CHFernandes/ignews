b12f61a0d79e735091d930c919875d38bc3fa821
Carlos Fernandes <chfernandes01@hotmail.com>
2022-03-09 19:47:39 -0300

    Chapter 3 - Module 2 - NextJs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,29 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from './home.module.scss';
+import avatar from '../../public/images/avatar.svg';
+import { SubscribeButton } from '../components/SubscribeButton';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>Hello World</h1>
+      
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get Access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <Image src={avatar} alt="Girl coding" />
+      </main>
     </>
     
   )
