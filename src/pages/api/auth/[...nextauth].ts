1e980b5d650db7fc6d434037f45169016b637810
Carlos Fernandes <chfernandes01@hotmail.com>
2022-03-21 22:48:09 -0300

    Chapter 3 Module 2 - Backend

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
-import NextAuth from 'next-auth';
+import NextAuth, { Account, Profile, User } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import { query as q } from 'faunadb';
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -13,4 +15,38 @@ export default NextAuth({
       }
     }),
   ],
+  callbacks: {
+    async signIn({user, account, profile}: {user: User, account: Account, profile: Profile}) {
+      const { email } = user;
+
+      try{
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email }}
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        );
+
+        return true;
+      } catch{
+        return false;
+      }
+    },
+  }
 });
